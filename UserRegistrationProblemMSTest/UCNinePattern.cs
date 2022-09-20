62940bd6dc003bd5133841d5cafd1a55aceafd3e
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-20 20:18:55 +0530

    [Shivani] UC9 - Validate Email samples

---
+++ UserRegistrationProblemMSTest/UCNinePattern.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblemMSTest
+{
+    public class UCNinePattern
+    {
+        public string email;
+
+        public UCNinePattern(string email)
+        {
+            this.email = email;
+        }
+
+        public string ValidateEmail()
+        {
+            string pattern = @"^([a-z]+)(\.[a-z0-9_\+\-]+)?@([a-z]+)\.([a-z]{2,4})(\.[a-z]{2})?$";
+
+            if (Regex.IsMatch(email, pattern))
+            {
+                return email;
+            }
+            else
+            {
+                return null;
+            }
+            if (Regex.IsMatch(email, pattern))
+            {
+                return email;
+            }
+            else
+            {   
+                return null;
+            }
+        }
+    }
+}
