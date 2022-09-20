8c91686912776eedd80137639d081ef7792dcc51
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-20 18:05:25 +0530

    [Shivani] UC1 - Valid First Name

---
+++ UserRegistrationProblemMSTest/UCOnePattern.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblemMSTest
+{
+    public class UCOnePattern
+    {
+        public string firstName;
+
+        public UCOnePattern(string firstName)
+        {
+            this.firstName = firstName;
+        }
+    
+        public string ValidateFirstName()
+        {
+
+            string pattern = "^[A-Z]{1}[a-z]{2,}$";
+            if (Regex.IsMatch(firstName, pattern))
+            {
+                return firstName;
+            }
+            else
+            {
+                return firstName;
+            }
+        }
+    }
+}
